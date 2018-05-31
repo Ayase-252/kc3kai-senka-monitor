ee6c475a3707255fd675aad2ebb56c07e435eac7
Qingyu Deng <bitdqy@hotmail.com>
2018-05-31 20:42:48 +0800

    Complete the feature to record rank pts on a daily basis

---
+++ senka-monitor.js
@@ -0,0 +1,161 @@
+const SenkaMonitor = {
+    expPrevDayCutoff: 0,
+    expPrevMonthCutoff: 0,
+    expCurrent: 0,
+    currentTime: 0,
+    timeNextDayCutoff: 0,
+    timeNextMonthCutoff: 0,
+    rankPtsGainPerDay: [],
+
+    setDayCutoffExp: function (exp) {
+        this.expPrevDayCutoff = exp;
+    },
+
+    setMonthCutoffExp: function (exp) {
+        this.expPrevMonthCutoff = exp;
+    },
+
+    setCurrentExp: function (exp) {
+        this.expCurrent = exp;
+    },
+
+    getRankPtsGainSinceMonthCutoff: function () {
+        return this._calculateRankPtsGain(this.expCurrent,
+            this.expPrevMonthCutoff);
+    },
+
+    getRankPtsGainSinceDayCutoff: function () {
+        return this._calculateRankPtsGain(this.expCurrent,
+            this.expPrevDayCutoff);
+    },
+
+    _calculateRankPtsGain: function (currExp, cutoffExp) {
+        return (currExp - cutoffExp) / 1428;
+    },
+
+    setCurrentTime: function (time) {
+        this.currentTime = time;
+    },
+
+    getNextDayCutoffTime: function () {
+        const now = new Date(this.currentTime);
+        // Cutoff on 1700(UTC) everyday
+        // If it is the last day of a month, cutoff will be at 2200(JST), 
+        // 1300(UTC)
+        let nextDay = now.getUTCDate();
+        let cutoffHour = 17;
+        if (now.getUTCHours() >= 17 ||
+            (now.getUTCDate() === this._getMonthLastDay() &&
+                now.getUTCHours() > 13)) {
+            nextDay += 1;
+        }
+        if (nextDay === this._getMonthLastDay()) {
+            cutoffHour = 13;
+        }
+        return Date.UTC(now.getUTCFullYear(), now.getUTCMonth(),
+            nextDay, cutoffHour);
+    },
+
+    getNextMonthCutoffTime: function () {
+        const now = new Date(this.currentTime);
+        const currentYear = now.getUTCFullYear();
+        const currentMonth = now.getUTCMonth();
+        // It will be 13:00 of the last day of specified month (UTC).
+        let proposedCutoffTime = Date.UTC(currentYear, currentMonth + 1, 0, 13);
+        if (now.valueOf() > proposedCutoffTime) {
+            proposedCutoffTime = Date.UTC(currentYear, currentMonth + 2, 0, 13);
+        }
+        return proposedCutoffTime;
+    },
+
+    setNextDayCutoffTime: function (time) {
+        this.timeNextDayCutoff = time;
+    },
+
+    setNextMonthCutoffTime: function (time) {
+        this.timeNextMonthCutoff = time;
+    },
+
+    getPrevDayCutoffExp: function () {
+        return this.expPrevDayCutoff;
+    },
+
+    getPrevMonthCutoffExp: function () {
+        return this.expPrevMonthCutoff;
+    },
+
+    getRankPtsGainPerDay: function () {
+        return this.rankPtsGainPerDay;
+    },
+
+    setPrevDayCutoffExp: function (exp) {
+        this.expPrevDayCutoff = exp;
+    },
+
+    getDailyRankPtsGain: function (day) {
+        return this.rankPtsGainPerDay[day - 1];
+    },
+
+    checkDayCutoff: function () {
+        if (this.currentTime > this.timeNextDayCutoff) {
+            const dayIndex = new Date(this.timeNextDayCutoff).getUTCDate() - 1;
+            this.rankPtsGainPerDay[dayIndex] = this._calculateRankPtsGain(
+                this.expCurrent, this.expPrevDayCutoff);
+            this.expPrevDayCutoff = this.expCurrent;
+            this.timeNextDayCutoff = this.getNextDayCutoffTime();
+        }
+    },
+
+    checkMonthCutoff: function () {
+        if (this.currentTime > this.timeNextMonthCutoff) {
+            this.expPrevMonthCutoff = this.expCurrent;
+            this.timeNextMonthCutoff = this.getNextMonthCutoffTime();
+            this.rankPtsGainPerDay = Array(this._getMonthLastDay()).fill(0);
+
+            this.expPrevDayCutoff = this.expCurrent;
+            this.timeNextDayCutoff = this.getNextDayCutoffTime();
+        }
+    },
+
+    _getMonthLastDay: function () {
+        // UTC+9, now UTC of jstNow is the JST
+        const utcNow = new Date(this.currentTime);
+        const jstNow = new Date(utcNow + 9 * 3600000);
+        const lastDay = new Date(Date.UTC(jstNow.getUTCFullYear(),
+            jstNow.getUTCMonth() + 1, 0)).getUTCDate();
+        return lastDay;
+    },
+
+    saveTo: function(localStorage) {
+        const dataObj = {
+            expPrevDayCutoff: this.expPrevDayCutoff,
+            expPrevMonthCutoff: this.expPrevMonthCutoff,
+            expCurrent: this.expCurrent,
+            currentTime: this.currentTime,
+            timeNextDayCutoff: this.timeNextDayCutoff,
+            timeNextMonthCutoff: this.timeNextMonthCutoff,
+            rankPtsGainPerDay: this.rankPtsGainPerDay,
+        };
+        localStorage.senkamonitor = JSON.stringify(dataObj);
+    },
+
+    readFrom: function(localStorage) {
+        if(localStorage.senkamonitor !== undefined) {
+            const parsedDataObj = JSON.parse(localStorage.senkamonitor);
+            this.expPrevDayCutoff = parsedDataObj.expPrevDayCutoff;
+            this.expPrevMonthCutoff = parsedDataObj.expPrevDayCutoff;
+            this.expCurrent = parsedDataObj.expCurrent;
+            this.currentTime = parsedDataObj.currentTime;
+            this.timeNextDayCutoff = parsedDataObj.timeNextDayCutoff;
+            this.timeNextMonthCutoff = parsedDataObj.timeNextMonthCutoff;
+            this.rankPtsGainPerDay = parsedDataObj.rankPtsGainPerDay;
+        };
+    },
+    
+    getCurrentExp() {
+        return this.expCurrent;   
+    }
+};
+
+
+exports.SenkaMonitor = SenkaMonitor;
---
+++ test/senka-monitor.spec.js
@@ -0,0 +1,103 @@
+const SenkaMonitor = require('../senka-monitor').SenkaMonitor;
+const expect = require('chai').expect;
+
+describe("SenkaMonitor", () => {
+    it("should calculate rank pts based on previous cutoff.", () => {
+        SenkaMonitor.setDayCutoffExp(19420000);
+        SenkaMonitor.setMonthCutoffExp(19000000);
+
+        SenkaMonitor.setCurrentExp(19500000);
+        expect(SenkaMonitor.getRankPtsGainSinceMonthCutoff()).be.closeTo(350.14, 0.01);
+        expect(SenkaMonitor.getRankPtsGainSinceDayCutoff()).be.closeTo(56.02, 0.01);
+    });
+
+    describe('Day cutoff', () => {
+        context('if it is not the last day of a month', () => {
+            it('should happen after 1700z everyday', () => {
+                SenkaMonitor.setCurrentTime(Date.UTC(2018, 5, 20, 14));
+                expect(SenkaMonitor.getNextDayCutoffTime()).to.equal(
+                    Date.UTC(2018, 5, 20, 17)
+                );
+
+                SenkaMonitor.setCurrentTime(Date.UTC(2018, 5, 20, 18));
+                expect(SenkaMonitor.getNextDayCutoffTime()).to.equal(
+                    Date.UTC(2018, 5, 21, 17)
+                );
+            });
+        });
+        context('if it is the last day of a month', () => {
+            it('should happen after 1300z', () => {
+                SenkaMonitor.setCurrentTime(Date.UTC(2018, 0, 30, 22));
+                expect(SenkaMonitor.getNextDayCutoffTime()).to.equal(
+                    Date.UTC(2018, 0, 31, 13)
+                );
+                SenkaMonitor.setCurrentTime(Date.UTC(2018, 1, 28, 11));
+                expect(SenkaMonitor.getNextDayCutoffTime()).to.equal(
+                    Date.UTC(2018, 1, 28, 13)
+                );
+            });
+        });
+
+        it('should execute day cutoff if current time is later than scheduled '
+            + 'day cutoff time', () => {
+                SenkaMonitor.setCurrentTime(Date.UTC(2018, 4, 20, 14, 2, 1));
+                SenkaMonitor.setNextDayCutoffTime(Date.UTC(2018, 4, 20, 17));
+                
+                SenkaMonitor.setCurrentTime(Date.UTC(2018, 4, 20, 15));
+                SenkaMonitor.checkMonthCutoff();
+                SenkaMonitor.checkDayCutoff();
+                
+                expect(SenkaMonitor.getNextDayCutoffTime()).to.equal(
+                    Date.UTC(2018, 4, 20, 17)
+                );
+                
+                SenkaMonitor.setPrevDayCutoffExp(648204);
+                SenkaMonitor.setCurrentTime(Date.UTC(2018, 4, 20, 18, 0, 4));
+                SenkaMonitor.setCurrentExp(649094);
+                SenkaMonitor.checkDayCutoff();
+
+                expect(SenkaMonitor.getNextDayCutoffTime()).to.equal(
+                    Date.UTC(2018, 4, 21, 17)
+                );
+                expect(SenkaMonitor.getPrevDayCutoffExp()).to.equal(649094);
+                expect(SenkaMonitor.getDailyRankPtsGain(20)).closeTo(0.62, 0.01);
+            });
+    });
+
+    describe('Month cutoff', () => {
+        it('should happened after 1300z of the last day of the month', () => {
+            SenkaMonitor.setCurrentTime(Date.UTC(2018, 4, 30));
+
+            expect(SenkaMonitor.getNextMonthCutoffTime()).to.equal(
+                Date.UTC(2018, 4, 31, 13)
+            );
+
+            SenkaMonitor.setCurrentTime(Date.UTC(2018, 4, 31, 14));
+            expect(SenkaMonitor.getNextMonthCutoffTime()).to.equal(
+                Date.UTC(2018, 5, 30, 13)
+            );
+
+            SenkaMonitor.setCurrentTime(Date.UTC(2018, 11, 31, 15));
+            expect(SenkaMonitor.getNextMonthCutoffTime()).to.equal(
+                Date.UTC(2019, 0, 31, 13)
+            );
+        });
+
+        it('should execute month cutoff if the time is later than scheduled ' +
+            'cut off time', () => {
+            SenkaMonitor.setCurrentExp(597402);
+            SenkaMonitor.setNextMonthCutoffTime(Date.UTC(2018, 0, 31, 13));
+            SenkaMonitor.setCurrentTime(Date.UTC(2018, 1, 1));
+
+            SenkaMonitor.checkMonthCutoff();
+            expect(SenkaMonitor.getPrevMonthCutoffExp()).to.equal(597402);
+            expect(SenkaMonitor.getRankPtsGainPerDay()).to.deep.equal(
+                Array(28).fill(0));
+            // It should also reset the day cutoff time;
+            expect(SenkaMonitor.getNextDayCutoffTime()).to.equal(
+                Date.UTC(2018, 1, 1, 17)
+            );
+            expect(SenkaMonitor.getPrevDayCutoffExp()).to.equal(597402);
+        });
+    });
+});
